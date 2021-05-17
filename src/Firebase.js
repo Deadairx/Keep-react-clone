dcfa1b092f7be3913679658e66d1925e80755229
Cody Arnold <carnold@bluebeam.com>
2021-05-17 10:12:10 -0500

    Add basic layout

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCz60BbC9oTOTi-2_AuhxuqIO3aXzN9SiI",
+authDomain: "keep-react-clone-277b3.firebaseapp.com",
+projectId: "keep-react-clone-277b3",
+storageBucket: "keep-react-clone-277b3.appspot.com",
+messagingSenderId: "961399176705",
+appId: "1:961399176705:web:aeefea7f2ed00f95118a00"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import keepLogo from '../assets/keep-logo.png'
+import reactLogo from '../logo.svg'
+import firebaseLogo from '../assets/firebase-logo.png'
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px 25px;
+  border-bottom: 1px solid rgba(60, 64, 67, 0.2);
+  `;
+
+const ImgWrap = styled.div`
+  display: flex;
+  align-items: center;
+  `;
+
+const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  `;
+
+const Header = () => {
+    return (
+      <Nav>
+        <p>Keep clone</p>
+        <ImgWrap>
+          <Img src={keepLogo} alt="Google keep logo" />
+          <p>+</p>
+          <Img src={reactLogo} alt="React logo" />
+          <p>+</p>
+          <Img src={firebaseLogo} alt="firebase logo" />
+        </ImgWrap>
+      </Nav>
+    );
+};
+
+export default Header;
